e1b7ada8b80bf75cda2f10146c779baebb553c72
Alexander Demin <alexander.demin@wonga.com>
2017-01-13 11:19:05 +0000

    Update BlobBuilder

--- js/BlobBuilder.js
+++ js/BlobBuilder.js
@@ -1,34 +1,22 @@
 /* BlobBuilder.js
- * A BlobBuilder implementation.
- * 2012-04-21
- * 
- * By Eli Grey, http://eligrey.com
- * License: X11/MIT
- *   See LICENSE.md
+ * A complete BlobBuilder shim
+ * By Eli Grey
+ * License: MIT/X11
  */
 
-/*global self, unescape */
-/*jslint bitwise: true, regexp: true, confusion: true, es5: true, vars: true, white: true,
-  plusplus: true */
-
-/*! @source http://purl.eligrey.com/github/BlobBuilder.js/blob/master/BlobBuilder.js */
-
-var BlobBuilder = BlobBuilder || self.WebKitBlobBuilder || self.MozBlobBuilder || self.MSBlobBuilder || (function(view) {
+self.BlobBuilder || self.WebKitBlobBuilder || self.MozBlobBuilder || function(view) {
 "use strict";
 var
 	  get_class = function(object) {
 		return Object.prototype.toString.call(object).match(/^\[object\s(.*)\]$/)[1];
 	}
-	, FakeBlobBuilder = function(){
-		this.data = [];
-	}
-	, FakeBlob = function(data, type, encoding) {
+	, FakeBlobBuilder = view.BlobBuilder = function(){}
+	, FakeBlob = view.Blob = function(data, type) {
 		this.data = data;
 		this.size = data.length;
 		this.type = type;
-		this.encoding = encoding;
 	}
-	, FBB_proto = FakeBlobBuilder.prototype
+	, FBB_proto = FakeBlobBuilder.prototype = []
 	, FB_proto = FakeBlob.prototype
 	, FileReaderSync = view.FileReaderSync
 	, FileException = function(type) {
@@ -39,64 +27,53 @@ var
 		+ "NO_MODIFICATION_ALLOWED_ERR INVALID_STATE_ERR SYNTAX_ERR"
 	).split(" ")
 	, file_ex_code = file_ex_codes.length
-	, realURL = view.URL || view.webkitURL || view
-	, real_create_object_URL = realURL.createObjectURL
-	, real_revoke_object_URL = realURL.revokeObjectURL
-	, URL = realURL
+	, URL = view.URL = view.URL || view.webkitURL || view
+	, real_create_object_url
+	, real_revoke_object_url
 	, btoa = view.btoa
-	, atob = view.atob
+	, ArrayBuffer = view.ArrayBuffer
 	, can_apply_typed_arrays = false
 	, can_apply_typed_arrays_test = function(pass) {
 		can_apply_typed_arrays = !pass;
 	}
-	
-	, ArrayBuffer = view.ArrayBuffer
-	, Uint8Array = view.Uint8Array
 ;
-FakeBlobBuilder.fake = FB_proto.fake = true;
 while (file_ex_code--) {
 	FileException.prototype[file_ex_codes[file_ex_code]] = file_ex_code + 1;
 }
 try {
-	if (Uint8Array) {
+	if (ArrayBuffer) {
 		can_apply_typed_arrays_test.apply(0, new Uint8Array(1));
 	}
 } catch (ex) {}
-if (!realURL.createObjectURL) {
-	URL = view.URL = {};
+if (!URL.createObjectURL) {
+	URL = {};
 }
+real_create_object_url = URL.createObjectURL;
+real_revoke_object_url = URL.revokeObjectURL;
 URL.createObjectURL = function(blob) {
-	var
-		  type = blob.type
-		, data_URI_header
-	;
+	var type = blob.type;
 	if (type === null) {
 		type = "application/octet-stream";
 	}
 	if (blob instanceof FakeBlob) {
-		data_URI_header = "data:" + type;
-		if (blob.encoding === "base64") {
-			return data_URI_header + ";base64," + blob.data;
-		} else if (blob.encoding === "URI") {
-			return data_URI_header + "," + decodeURIComponent(blob.data);
-		} if (btoa) {
-			return data_URI_header + ";base64," + btoa(blob.data);
+		if (btoa) {
+			return "data:" + type + ";base64," + btoa(blob.data);
 		} else {
-			return data_URI_header + "," + encodeURIComponent(blob.data);
+			return "data:" + type + "," + encodeURIComponent(blob.data);
 		}
 	} else if (real_create_object_url) {
-		return real_create_object_url.call(realURL, blob);
+		return real_create_object_url.call(URL, blob);
 	}
 };
 URL.revokeObjectURL = function(object_url) {
 	if (object_url.substring(0, 5) !== "data:" && real_revoke_object_url) {
-		real_revoke_object_url.call(realURL, object_url);
+		real_revoke_object_url.call(URL, object_url);
 	}
 };
 FBB_proto.append = function(data/*, endings*/) {
-	var bb = this.data;
+	var bb = this;
 	// decode data to a binary string
-	if (Uint8Array && data instanceof ArrayBuffer) {
+	if (ArrayBuffer && data instanceof ArrayBuffer) {
 		if (can_apply_typed_arrays) {
 			bb.push(String.fromCharCode.apply(String, new Uint8Array(data)));
 		} else {
@@ -119,13 +96,7 @@ FBB_proto.append = function(data/*, endings*/) {
 			throw new FileException("NOT_READABLE_ERR");
 		}
 	} else if (data instanceof FakeBlob) {
-		if (data.encoding === "base64" && atob) {
-			bb.push(atob(data.data));
-		} else if (data.encoding === "URI") {
-			bb.push(decodeURIComponent(data.data));
-		} else if (data.encoding === "raw") {
-			bb.push(data.data);
-		}
+		bb.push(data.data);
 	} else {
 		if (typeof data !== "string") {
 			data += ""; // convert unsupported types to strings
@@ -138,7 +109,7 @@ FBB_proto.getBlob = function(type) {
 	if (!arguments.length) {
 		type = null;
 	}
-	return new FakeBlob(this.data.join(""), type, "raw");
+	return new FakeBlob(this.join(""), type);
 };
 FBB_proto.toString = function() {
 	return "[object BlobBuilder]";
@@ -151,11 +122,10 @@ FB_proto.slice = function(start, end, type) {
 	return new FakeBlob(
 		  this.data.slice(start, args > 1 ? end : this.data.length)
 		, type
-		, this.encoding
 	);
 };
 FB_proto.toString = function() {
 	return "[object Blob]";
 };
 return FakeBlobBuilder;
-}(self));
+}(self);
