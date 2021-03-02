055b17931dacf1a5ad89969573d936d210588796
Alexander Demin <alexander@demin.ws>
2021-03-02 13:53:03 +0000

    Re-formst ui.js

--- docs/ui.js
+++ docs/ui.js
@@ -17,7 +17,6 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 function UI(tape_catalog, runner, memory, autoexec) {
-
   this.tape_catalog = tape_catalog;
   this.runner = runner;
   this.memory = memory;
@@ -34,24 +33,26 @@ function UI(tape_catalog, runner, memory, autoexec) {
   this.memory_snapshot_count = 1;
 
   if (!this.canvas.getContext) {
-    alert("Tag <canvas> is not support is the browser")
+    alert("Tag <canvas> is not support is the browser");
     return;
   }
 
-  if (navigator.userAgent.toLowerCase().indexOf('chrome') < 0) {
-    document.getElementById('assembler_frame').style.width = (window.innerWidth - 32) + "px";
-    document.getElementById('assembler_frame').style.height = (window.innerHeight - 50) + "px";
+  if (navigator.userAgent.toLowerCase().indexOf("chrome") < 0) {
+    document.getElementById("assembler_frame").style.width =
+      window.innerWidth - 32 + "px";
+    document.getElementById("assembler_frame").style.height =
+      window.innerHeight - 50 + "px";
   }
 
-  this.file_selector = document.getElementById('file_selector');
+  this.file_selector = document.getElementById("file_selector");
 
   for (var i in this.tape_catalog) {
     var name = this.tape_catalog[i];
     this.file_selector.add(new Option(name, name), null);
   }
 
-  this.ips = document.getElementById('ips');
-  this.tps = document.getElementById('tps');
+  this.ips = document.getElementById("ips");
+  this.tps = document.getElementById("tps");
   var update_perf_this = this;
   this.update_perf = function () {
     function update(element, value) {
@@ -59,20 +60,20 @@ function UI(tape_catalog, runner, memory, autoexec) {
     }
     update(ips, update_perf_this.runner.instructions_per_millisecond);
     update(tps, update_perf_this.runner.ticks_per_millisecond);
-  }
+  };
   setInterval(this.update_perf, 2000);
 
   this.sound_toggle = function (checkbox) {
     this.runner.init_sound(checkbox.checked);
-  }
+  };
 
   this.resize_screen = function () {
-    var width = document.getElementById('screen_width').value;
-    var height = document.getElementById('screen_height').value;
-    var scale_x = document.getElementById('scale_x').value;
-    var scale_y = document.getElementById('scale_y').value;
+    var width = document.getElementById("screen_width").value;
+    var height = document.getElementById("screen_height").value;
+    var scale_x = document.getElementById("scale_x").value;
+    var scale_y = document.getElementById("scale_y").value;
     screen.set_view(width, height, scale_x, scale_y);
-  }
+  };
 
   this.resize_canvas = function (width, height) {
     this.canvas.width = width;
@@ -80,13 +81,13 @@ function UI(tape_catalog, runner, memory, autoexec) {
 
     this.panel.width = this.canvas.width + 4;
     this.panel.height = this.canvas.height + 4;
-  }
+  };
 
   this.reset = function () {
     this.runner.cpu.memory.keyboard.reset();
     this.runner.cpu.jump(0xf800);
     console.log("Reset");
-  }
+  };
 
   this.restart = function () {
     this.runner.cpu.memory.zero_ram();
@@ -97,29 +98,27 @@ function UI(tape_catalog, runner, memory, autoexec) {
       restart_this.autorun();
       restart_this.autorun_executed = true;
     }, 1000);
-  }
+  };
 
   this.update_pause_button = function (paused) {
-    var button = document.getElementById("pause_button")
+    var button = document.getElementById("pause_button");
 
     button.innerHTML = paused ? "Resume" : "Pause";
     button.style.background = paused ? "red" : "";
-  }
+  };
 
   this.pause = function () {
     if (this.runner.paused) {
       this.runner.resume();
       console.log("Resumed");
-      if (this.console_window)
-        this.console_window.console.resume_ui_callback();
+      if (this.console_window) this.console_window.console.resume_ui_callback();
     } else {
       this.runner.pause();
       console.log("Paused at " + this.runner.cpu.pc.toString(16));
-      if (this.console_window)
-        this.console_window.console.pause_ui_callback();
+      if (this.console_window) this.console_window.console.pause_ui_callback();
     }
     this.update_pause_button(this.runner.paused);
-  }
+  };
 
   this.tape_file_name = function (name) {
     return "files/" + name;
@@ -129,31 +128,31 @@ function UI(tape_catalog, runner, memory, autoexec) {
     var load_tape_file_this = this;
     var callback = function (image) {
       load_tape_file_this.file_loaded(name, image);
-    }
+    };
     GetBinaryFile(this.tape_file_name(name), callback, true);
-  }
+  };
 
   this.selected_file_name = function () {
     return this.file_selector.options[this.file_selector.selectedIndex].value;
-  }
+  };
 
   this.run_selected = function () {
     this.load_mode = "run";
     this.load_tape_file(this.selected_file_name());
-  }
+  };
 
   this.load_selected = function () {
     this.load_mode = "load";
     this.load_tape_file(this.selected_file_name());
-  }
+  };
 
   this.disassembler_available = function () {
     return window.frames.disassembler_frame.loaded;
-  }
+  };
 
   this.extract_rk86_word = function (v, i) {
     return ((v.charCodeAt(i) & 0xff) << 8) | (v.charCodeAt(i + 1) & 0xff);
-  }
+  };
 
   this.parse_rk86_binary = function (name, image) {
     var file = {};
@@ -175,16 +174,16 @@ function UI(tape_catalog, runner, memory, autoexec) {
       file.size = file.end - file.start + 1;
       file.image = v.substr(i, file.size);
     }
-    file.entry = (name == "PVO.GAM" ? 0x3400 : file.start);
+    file.entry = name == "PVO.GAM" ? 0x3400 : file.start;
     return file;
-  }
+  };
 
   this.autorun = function () {
     if (this.autoexec.file) {
       this.load_mode = this.autoexec.loadonly ? "load" : "run";
       this.load_tape_file(this.autoexec.file);
     }
-  }
+  };
 
   this.file_loaded = function (name, binary) {
     if (binary == null) {
@@ -209,15 +208,24 @@ function UI(tape_catalog, runner, memory, autoexec) {
 
     if (this.load_mode == "load") {
       var sz = file.start + file.image.length - 1;
-      alert("Loaded: " + file.name +
-        "(" + file.start.toString(16) + "-" + sz.toString(16) + "), " +
-        "Run by 'G" + file.entry.toString(16) + "'");
+      alert(
+        "Loaded: " +
+          file.name +
+          "(" +
+          file.start.toString(16) +
+          "-" +
+          sz.toString(16) +
+          "), " +
+          "Run by 'G" +
+          file.entry.toString(16) +
+          "'"
+      );
     } else {
       console.log("Started", file.name, "from", file.entry.toString(16));
       screen.init_cache();
       this.runner.cpu.jump(file.entry);
     }
-  }
+  };
 
   this.switch_panel = function (name) {
     document.getElementById("emulator_panel").style.display =
@@ -233,10 +241,14 @@ function UI(tape_catalog, runner, memory, autoexec) {
     if (name == "assembler" || name == "keyboard") {
       var frame_name = name + "_frame";
       var panel_name = name + "_panel";
-      document.getElementById(frame_name).style.width = document.getElementById(panel_name).offsetWidth;
-      document.getElementById(frame_name).style.height = document.getElementById(panel_name).offsetHeight;
+      document.getElementById(frame_name).style.width = document.getElementById(
+        panel_name
+      ).offsetWidth;
+      document.getElementById(
+        frame_name
+      ).style.height = document.getElementById(panel_name).offsetHeight;
     }
-  }
+  };
 
   this.toggle_panel = function (name) {
     if (name == "disassembler" && !this.disassembler_available()) {
@@ -246,11 +258,11 @@ function UI(tape_catalog, runner, memory, autoexec) {
     var name = name + "_panel";
     document.getElementById(name).style.display =
       document.getElementById(name).style.display == "block" ? "none" : "block";
-  }
+  };
 
   this.disassembler_available = function () {
     return window.frames.disassembler_frame.loaded;
-  }
+  };
 
   this.save_screen = function () {
     var filename = this.screenshot_name + "-" + this.screenshot_count + ".png";
@@ -258,7 +270,7 @@ function UI(tape_catalog, runner, memory, autoexec) {
     this.canvas.toBlob(function (blob) {
       saveAs(blob, filename);
     });
-  }
+  };
 
   this.save_memory = function () {
     var snapshot = new Uint8Array(this.memory.snapshot(0, 0x10000));
@@ -269,31 +281,40 @@ function UI(tape_catalog, runner, memory, autoexec) {
     );
     saveAs(snapshot_blob, filename);
     this.memory_snapshot_count += 1;
-  }
+  };
 
   this.console = function () {
-    this.console_window = window.open("console.html", '_blank',
-      'toolbar=yes, location=yes, status=no, menubar=yes, scrollbars=yes, ' +
-      'resizable=yes, width=700, height=600');
-  }
+    this.console_window = window.open(
+      "console.html",
+      "_blank",
+      "toolbar=yes, location=yes, status=no, menubar=yes, scrollbars=yes, " +
+        "resizable=yes, width=700, height=600"
+    );
+  };
 
   this.visualizer = function () {
-    this.visualizer_window = window.open("i8080_visualizer.html", '_blank',
-      'toolbar=yes, location=yes, status=no, menubar=yes, scrollbars=yes, ' +
-      'resizable=yes, width=700, height=600');
-  }
+    this.visualizer_window = window.open(
+      "i8080_visualizer.html",
+      "_blank",
+      "toolbar=yes, location=yes, status=no, menubar=yes, scrollbars=yes, " +
+        "resizable=yes, width=700, height=600"
+    );
+  };
 
   this.clear_selection = function () {
     if (window.getSelection) {
-      if (window.getSelection().empty) {  // Chrome
+      if (window.getSelection().empty) {
+        // Chrome
         window.getSelection().empty();
-      } else if (window.getSelection().removeAllRanges) {  // Firefox
+      } else if (window.getSelection().removeAllRanges) {
+        // Firefox
         window.getSelection().removeAllRanges();
       }
-    } else if (document.selection) {  // IE?
+    } else if (document.selection) {
+      // IE?
       document.selection.empty();
     }
-  }
+  };
 
   this.fullscreen = function () {
     if (!this.canvas.fullscreen) {
@@ -327,13 +348,15 @@ function UI(tape_catalog, runner, memory, autoexec) {
       fullscreen_this.fullscreen_off();
       fullscreen_this.clear_selection();
       return false;
-    }
-    window.onresize = function () { fullscreen_this.fullscreen(); }
-  }
+    };
+    window.onresize = function () {
+      fullscreen_this.fullscreen();
+    };
+  };
 
   this.fullscreen_off = function () {
     this.canvas.fullscreen = false;
-    window.onresize = function () { };
+    window.onresize = function () {};
     this.canvas.style.width = this.canvas.normal_width + "px";
     this.canvas.style.height = this.canvas.normal_height + "px";
     this.canvas.style.position = "static";
@@ -342,7 +365,7 @@ function UI(tape_catalog, runner, memory, autoexec) {
     this.fullscreen_panel.style.visibility = "hidden";
     this.panel.appendChild(this.canvas);
     this.panel.style.visibility = "visible";
-  }
+  };
 
   this.load_mode = "run";
   this.load_tape_file("mon32.bin");
