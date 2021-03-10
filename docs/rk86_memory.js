53479f7501ffd7d266c357e7e0d01faa78eec449
Alexander Demin <alexander@demin.ws>
2021-03-10 12:18:47 +0000

    Release

--- docs/rk86_memory.js
+++ docs/rk86_memory.js
@@ -98,7 +98,7 @@ function Memory(keyboard) {
 
     if (addr == 0xc001) {
       return 0xff | ~(this.screen.light_pen_active ? 0x10 : 0x00);
-    };
+    }
 
     if (addr == 0xc000) {
       if (this.vg75_c001_60_cmd == 1) {
