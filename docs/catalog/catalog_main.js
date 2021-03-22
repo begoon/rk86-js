77bdcdbf3df5ba04334df880be87d920ecca8692
Alexander Demin <alexander@demin.ws>
2021-03-22 16:06:31 +0000

    Release updated catalog

--- docs/catalog/catalog_main.js
+++ docs/catalog/catalog_main.js
@@ -6,64 +6,56 @@ function main() {
 
   main_this = this;
 
-  var images = document.getElementsByTagName('img'); 
-  for(var i = 0; i < images.length; i++) {
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
     var img = images[i];
     if (img.getAttribute("class") == "_screen") {
-      img.onmouseover = function() {
+      img.onmouseover = function () {
         this.setAttribute("original_height", this.height);
         this.style.height = this.height * 2;
       };
-      img.onmouseout = function() {
+      img.onmouseout = function () {
         this.style.height = this.getAttribute("original_height");
       };
     }
   }
-  
-  var buttons = document.getElementsByTagName('button'); 
-  for(var i = 0; i < buttons.length; i++) {
+
+  var buttons = document.getElementsByTagName('button');
+  for (var i = 0; i < buttons.length; i++) {
     var button = buttons[i];
     if (button.getAttribute("class") == "run") {
-      button.onclick = function() {
+      button.onclick = function () {
         main_this.launcher.href = "../index.html?file=" + this.name;
         launcher.click();
       };
     }
     if (button.getAttribute("class") == "load") {
-      button.onclick = function() {
+      button.onclick = function () {
         main_this.launcher.href = "../index.html?loadonly=1&file=" + this.name;
         launcher.click();
       };
     }
   }
 
-  var as = document.getElementsByTagName('a'); 
-  for(var i = 0; i < as.length; i++) {
+  var as = document.getElementsByTagName('a');
+  for (var i = 0; i < as.length; i++) {
     var a = as[i];
     if (a.getAttribute("class") == "filter") {
       a.href = "#";
-      a.onclick = function() {
+      a.onclick = function () {
         search.value = this.innerHTML;
         search.onkeyup();
       };
     }
   }
-  
+
   var search = document.getElementById("search");
-  search.onclick = function() {
-    if (search.getAttribute("first_click") == "yes") {
-      search.setAttribute("first_click", null);
-      search.setAttribute("value", "");
-    }
-  }
-  search.setAttribute("value", "поиск");
-  search.setAttribute("first_click", "yes");
-  
-  search.onkeyup = function() {
+
+  search.onkeyup = function () {
     var needle = search.value.toUpperCase();
     var always = needle.length < 3;
-    var trs = document.getElementsByTagName('tr'); 
-    for(var i = 0; i < trs.length; i++) {
+    var trs = document.getElementsByTagName('tr');
+    for (var i = 0; i < trs.length; i++) {
       var tr = trs[i];
       if (tr.getAttribute("class") == "cart") {
         var name = tr.getAttribute("name");
@@ -75,4 +67,6 @@ function main() {
       }
     }
   }
+
+  search.onkeyup();
 }
