5e6b84c3ecf0f2158edc4cd75606302734ac09ca
Alexander Demin <alexander@demin.ws>
2021-11-28 08:13:56 +0000

    Add comparing binaries as words

--- cmp.py
+++ cmp.py
@@ -1,19 +1,30 @@
 import sys
 
-binaries = []
+binaries_8 = []
 
 sz = 0
 for f in sys.argv[1:]:
     print(f'@ {f}')
     with open(f, 'rb') as f:
-        binaries.append(f.read())
-    file_sz = len(binaries[-1])
+        binaries_8.append(f.read())
+    file_sz = len(binaries_8[-1])
     print(f'# {file_sz}')
     if sz == 0:
         sz = file_sz
     else:
         if sz != file_sz:
             raise ValueError(f'file {f} size is expected to be {sz}')
+
+
+def as_binary_16(binary):
+    sz = len(binary)
+    return [
+        binary[address] | (binary[(address + 1) % sz] << 8)
+        for address in range(0, sz)
+    ]
+
+
+binaries_16 = [as_binary_16(binary) for binary in binaries_8]
 
 
 def strictly_increasing(L):
@@ -28,19 +39,22 @@ def same(L):
     return all(x == y for x, y in zip(L, L[1:]))
 
 
-def print_diff(title: str, cmp):
+def print_diff(title: str, binaries, format, cmp):
     print(title)
     for offset in range(sz):
-        line = [b[offset] for b in binaries]
+        line = [b[offset] for b in binaries_8]
         if same(line):
             continue
         if not cmp(line):
             continue
         print(
             '%04X: %s'
-            % (offset, ' '.join(list(map(lambda a: '%02X' % a, line))))
+            % (offset, ' '.join(list(map(lambda a: format % a, line))))
         )
 
 
-print_diff('@ increments', strictly_increasing)
-print_diff('@ decrements', strictly_decreasing)
+print_diff('@ increments 8', binaries_8, '%02X', strictly_increasing)
+print_diff('@ decrements 8', binaries_8, '%02X', strictly_decreasing)
+
+print_diff('@ increments 16', binaries_16, '%04X', strictly_increasing)
+print_diff('@ decrements 16', binaries_16, '%04X', strictly_decreasing)
